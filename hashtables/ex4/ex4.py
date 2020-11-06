13f0af9ea21d844aca01f7b4b8bc7db1861083c5
hai666l <0xconr@protonmail.com>
2020-11-06 17:40:29 -0600

    ex4/

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,11 +1,15 @@
 def has_negatives(a):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
-
+    result = []
+    d = dict()
+    for n in a:
+        n = abs(n)
+        if n not in d:
+            d[n] = None
+        else:
+            del d[n]
+            result.append(n)
+            
     return result
-
 
 if __name__ == "__main__":
     print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
