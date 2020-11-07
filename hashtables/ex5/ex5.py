62ed4f90ef4b5d2f772bcb34c48afba6ac285d9e
hai666l <0xconr@protonmail.com>
2020-11-06 19:04:37 -0600

    ex5/ 2-3s runtime w/ hashtable

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,12 +1,20 @@
 def finder(files, queries):
-    result = []
+    d = dict()
+    for f in files:
+        for i in range(len(f)-1, 0, -1):
+            if f[i] == '/':
+                key = f[i+1:]
+                if key not in d.keys():
+                    d[key] = [f]
+                else:
+                    d[key].append(f)
+                break
+    r = []
     for q in queries:
-        for f in files:
-            if len(q) <= len(f):
-                if q == f[len(f)-len(q):]:
-                    result.append(f)
-    return result
-
+        if q in d.keys():
+            for s in d[q]:
+                r.append(s)
+    return r
 
 if __name__ == "__main__":
     files = [
